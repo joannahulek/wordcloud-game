cb4c564515e6c4ef7ad08f3b230577920f68101c
Tomasz Mojsiuk <tomasz.mojsiuk@gmail.com>
2026-06-15 22:05:50 +0200

    feat: 869d96eh2 trigger release

--- src/views/home-page/home-page.tsx
+++ src/views/home-page/home-page.tsx
@@ -1,7 +1,7 @@
 export function HomePage() {
     return (
         <div data-test="home-page">
-            <h1 data-test="home-page-header">Hello from home page</h1>
+            <h1 data-test="home-page-header">Wlecome to the worldcloud game!</h1>
         </div>
     )
 }
