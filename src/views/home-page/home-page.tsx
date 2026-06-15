2aa4f13a9d64a466cab7cc8336cae5c9af91b36a
Tomasz Mojsiuk <tomasz.mojsiuk@gmail.com>
2026-06-15 22:21:04 +0200

    feat: 869d96eh2 add title to main page (#14)
    feat: 869d96eh2 add title to main page (#14)
    BREAKING CHANGE: trigger first release

--- src/views/home-page/home-page.tsx
+++ src/views/home-page/home-page.tsx
@@ -1,7 +1,7 @@
 export function HomePage() {
     return (
         <div data-test="home-page">
-            <h1 data-test="home-page-header">Wlecome to the worldcloud game!</h1>
+            <h1 data-test="home-page-header">Wlecome to the  game!</h1>
         </div>
     )
 }
